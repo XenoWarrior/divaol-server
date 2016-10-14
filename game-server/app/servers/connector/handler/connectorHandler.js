fea9a89b352f6b842c395c222a9e44762ca140aa
XenoWarrior <admin@projectge.com>
2016-10-14 18:24:04 -0400

    Send back temporary user name and id

--- game-server/app/servers/connector/handler/connectorHandler.js
+++ game-server/app/servers/connector/handler/connectorHandler.js
@@ -7,13 +7,15 @@ var Handler = function(app) {
 };
 
 var handler = Handler.prototype;
+var tempID = 1;
 
 handler.admin = function(msg, session, next) {
 	next(null, {code: 200});
 };
 
 handler.login = function(msg, session, next) {
-	next(null, {code: 200});
+	next(null, {route: 'onEnter', code:200, msg:{uid: tempID.toString(), sid: "connector-server-1", username: "TestAccount_"+tempID.toString(), nickname: "TestAccount_"+tempID.toString(), stat:0}});
+	tempID++;
 };
 
 handler.logout = function(msg, session, next) {
